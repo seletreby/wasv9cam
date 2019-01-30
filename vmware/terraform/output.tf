ee4e052a438cc5cbc4820c4e50c4d9fb5e28864b
seletreby <salah@sa.ibm.com>
2019-01-30 18:37:32 +0100

    Remove Amazon and VMware

--- vmware/terraform/output.tf
+++
@@ -1,7 +0,0 @@
-output "WAS Admin Console DNS URL" {
-  value = "<a href='http://${var.WASNode01-name}:9060/ibm/console' target='_blank'>http://${var.WASNode01-name}:9060/ibm/console</a>"
-}
-
-output "WAS Admin Console IP URL" {
-  value = "<a href='http://${vsphere_virtual_machine.WASNode01.clone.0.customize.0.network_interface.0.ipv4_address}:9060/ibm/console' target='_blank'>http://${vsphere_virtual_machine.WASNode01.clone.0.customize.0.network_interface.0.ipv4_address}:9060/ibm/console</a>"
-}
